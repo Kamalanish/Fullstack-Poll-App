905d803d1b6cb686da42eb5bb9c572e56d7a8e3f
kamal <anishkamal200@gmail.com>
2024-11-10 20:30:57 +0530

    project structure fix

--- pollapp-be/src/main/java/com/example/PollingApp/PollingAppApplication.java
+++ pollapp-be/src/main/java/com/example/PollingApp/PollingAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.Polling.App;
+package com.example.PollingApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- pollapp-be/src/test/java/com/example/PollingApp/PollingAppApplicationTests.java
+++ pollapp-be/src/test/java/com/example/PollingApp/PollingAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Polling.App;
+package com.example.PollingApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
