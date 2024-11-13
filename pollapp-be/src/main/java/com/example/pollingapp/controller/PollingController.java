ee7cecc54bab349bbed40c8115fcaedba85ec23c
kamal <anishkamal200@gmail.com>
2024-11-13 23:07:03 +0530

    new api's implemented for polling app

--- pollapp-be/src/main/java/com/example/pollingapp/controller/PollingController.java
+++ pollapp-be/src/main/java/com/example/pollingapp/controller/PollingController.java
@@ -1,8 +1,10 @@
 package com.example.pollingapp.controller;
 
+import com.example.pollingapp.dto.Vote;
 import com.example.pollingapp.model.Poll;
 import com.example.pollingapp.servicce.PollService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +25,18 @@ public class PollingController {
     @GetMapping
     public List<Poll> getAllPolls() {
         return pollService.getAllPolls();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Poll> getAllPolls(@PathVariable Long id) {
+        return pollService.getPoolById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/vote")
+    public void votePoll(@RequestBody Vote vote){
+        pollService.vote(vote.getPollId(), vote.getOptionIndex());
     }
 
 
--- pollapp-be/src/main/java/com/example/pollingapp/servicce/PollService.java
+++ pollapp-be/src/main/java/com/example/pollingapp/servicce/PollService.java
@@ -1,11 +1,13 @@
 package com.example.pollingapp.servicce;
 
+import com.example.pollingapp.model.OptionVote;
 import com.example.pollingapp.model.Poll;
 import com.example.pollingapp.repostiory.PollRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +21,25 @@ public class PollService {
 
     public List<Poll> getAllPolls() {
         return pollRepository.findAll();
+    }
+
+    public Optional<Poll> getPoolById(Long id) {
+        return pollRepository.findById(id);
+    }
+
+    public void vote(Long pollId, int optionIndex) throws IllegalArgumentException {
+        Poll poll = pollRepository.findById(pollId)
+                .orElseThrow(() -> new IllegalArgumentException("Poll not found with id: " + pollId));
+
+        List<OptionVote> optionVotes = poll.getOptionVotes();
+        if (optionIndex < optionVotes.size()) {
+            OptionVote selectedOption = optionVotes.get(optionIndex);
+            selectedOption.setVote(selectedOption.getVote() + 1);
+            pollRepository.save(poll);
+        } else {
+            throw new IllegalArgumentException("Invalid option index");
+        }
+
+
     }
 }
