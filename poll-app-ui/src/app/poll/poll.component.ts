f33a6def68a5b9def9206737495d42d075d4a85c
kamal <anishkamal200@gmail.com>
2025-03-01 16:34:20 +0530

    feat: added vote, createPoll methods

--- poll-app-ui/src/app/poll/poll.component.ts
+++ poll-app-ui/src/app/poll/poll.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {PollService} from '../poll.service';
-import {Poll} from '../poll.models';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { PollService } from '../poll.service';
+import { Poll } from '../poll.models';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-poll',
@@ -12,6 +12,15 @@ import {FormsModule} from '@angular/forms';
   styleUrl: './poll.component.css'
 })
 export class PollComponent implements OnInit {
+
+  newPoll: Poll = {
+    id: 0,
+    questionText: '',
+    optionVotes: [
+      { optionText: '', vote: 0 },
+      { optionText: '', vote: 0 }
+    ]
+  };
 
   polls: Poll[] = [];
 
@@ -31,5 +40,51 @@ export class PollComponent implements OnInit {
         console.log("Error getting all Polls: ", error);
       }
     })
+  }
+
+  createPoll() {
+    this.pollService.createPoll(this.newPoll).subscribe({
+      next: (createdPoll) => {
+        this.polls.push(createdPoll);
+        this.resetPoll();
+      },
+      error: (error) => {
+        console.error("Error creating Polls: ", error);
+      }
+
+    })
+  }
+
+  addOption(){
+    this.newPoll.optionVotes.push({ optionText: '', vote: 0 });
+  }
+
+  resetPoll() {
+    this.newPoll = {
+      id: 0,
+      questionText: '',
+      optionVotes: [
+        { optionText: '', vote: 0 },
+        { optionText: '', vote: 0 }
+      ]
+    };
+  }
+
+  vote(pollId: number, optionIndex: number) {
+    this.pollService.vote(pollId, optionIndex).subscribe({
+      next: () => {
+        const poll = this.polls.find(poll => poll.id == pollId);
+        if(poll){
+          poll.optionVotes[optionIndex].vote++;
+        }
+      },
+      error: (error) => {
+        console.error("Error voting on Poll: ", error);
+      }
+    })
+  }
+
+  trackByIndex(index: number): number {
+    return index;
   }
 }
